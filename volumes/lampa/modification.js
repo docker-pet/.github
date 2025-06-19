6f42479875027626fae949ebb3b8fe6719efdacf
Jonas Otter <lawn-timber.0j@icloud.com>
2025-06-19 16:36:44 +0000

    feat: lampa auth

--- volumes/lampa/modification.js
+++ volumes/lampa/modification.js
@@ -1,9 +1,10 @@
 (async () => {
-    const url = new URL(location.href);
-    const parentDomain = url.hostname.replace(/^[^.]+\./, '');
+    const initializerVersion = 'v0.0.1';
+    let needToReload = false;
+
+    // Auth
     const retryLimit = 10;
     let retryCount = 0;
-
     while (true) {
         if (retryCount++ < retryLimit) {
             console.error('Retry limit reached, stopping the script.');
@@ -11,6 +12,9 @@
         }
 
         try {
+            const url = new URL(location.href);
+            const parentDomain = url.hostname.replace(/^[^.]+\./, '');
+
             const user = await fetch(`${url.protocol}//${parentDomain}/api/lampa/user`)
                 .then(response => response.json())
                 .catch(() => null);
@@ -22,7 +26,7 @@
                 console.infi('Device name or lampac unic ID mismatch.');
                 localStorage.setItem('device_name', user.deviceName);
                 localStorage.setItem('lampac_unic_id', user.authKey);
-                location.reload();
+                needToReload = true;
                 break;
             }
 
@@ -31,5 +35,24 @@
             console.error('Error in modification script:', e);
             await new Promise(resolve => setTimeout(resolve, 500));
         }
+    }
+
+    // Initializer
+    const userInitializerVersion = localStorage.getItem('lampa_modification_version')
+    if (userInitializerVersion !== initializerVersion) {
+        needToReload = true;
+        const lampaAccount = localStorage.getItem('account') || '{}';
+        const lampaAccountEmail = localStorage.getItem('account_email') || '';
+
+        localStorage.clear();
+        localStorage.setItem('account', lampaAccount);
+        localStorage.setItem('account_email', lampaAccountEmail);
+        localStorage.setItem('lampa_modification_version', initializerVersion);
+    }
+
+    // Reload if needed
+    if (needToReload) {
+        console.info('Reloading page due to changes.');
+        location.reload();
     }
 })();
